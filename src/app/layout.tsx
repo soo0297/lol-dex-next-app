1cad4de5dd5c9d98443a26858e35662b9ae35ddc
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-10-01 19:29:54 +0900

    layout: header, footer 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,26 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <header className="bg-gray-800 text-white w-full z-10 fixed top-0 py-4">
+          <nav className="container mx-auto flex justify-around text-xl font-bold">
+            <Link href={"/"}>홈</Link>
+            <Link href={"/champions"}>챔피언 목록</Link>
+            <Link href={"/items"}>아이템 목록</Link>
+            <Link href={"/rotation"}>챔피언 로테이션</Link>
+          </nav>
+        </header>
+        <main className="flex flex-col justify-center items-center">
+          {children}
+        </main>
+        <footer className="bg-gray-800 p-4 mt-8 fixed bottom-0 w-full">
+          <div className="container mx-auto text-center text-white text-sm">
+            [Your Product Name] is not endorsed by Riot Games and does not
+            reflect the views or opinions of Riot Games or anyone officially
+            involved in producing or managing Riot Games properties. Riot Games
+            and all associated properties are trademarks or registered
+            trademarks of Riot Games, Inc.
+          </div>
+        </footer>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <h1 className="text-red-500 text-4xl font-extrabold mt-28">
+        리그 오브 레전드 정보 앱
+      </h1>
+    </>
+  );
 }
