86ae1d36725112861a0c5f0b8aac65226f931d9d
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-10-01 20:53:21 +0900

    홈페이지 구조 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-red-500 text-4xl font-extrabold mt-28">
-        리그 오브 레전드 정보 앱
-      </h1>
+      <div className="text-center mt-28">
+        <h1 className="text-red-500 text-3xl font-extrabold ">
+          리그 오브 레전드 정보 앱
+        </h1>
+        <p className="mt-4 text-gray-500 mb-3">
+          Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.
+        </p>
+      </div>
+      <div className="flex flex-col justify-center items-center gap-2">
+        <Link href={"/champions"}>챔피언 목록보기</Link>
+        <Link href={"/rotation"}>금주 로테이션 확인</Link>
+        <Link href={"/items"}>아이템 목록 보기</Link>
+      </div>
     </>
   );
 }
