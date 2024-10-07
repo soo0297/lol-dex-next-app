4bbf17e187356a47281620b78555f36bcab07cc9
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-10-07 19:31:54 +0900

    상세페이지 css 수정

--- src/components/ChampionDetail.tsx
+++ src/components/ChampionDetail.tsx
@@ -9,12 +9,10 @@ const ChampionDetail = ({ data }: { data: Champion }) => {
       }}
     >
       <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-end pl-12 pb-11">
-        <h1 className="text-white text-4xl font-bold">{data.name}</h1>
-        <h3 className="text-white text-2xl">{data.title}</h3>
-        <p className="text-white text-base mt-4 max-w-2xl">{data.lore}</p>
-      </div>
-      <div>
-        <h3>스탯</h3>
+        <h1 className="text-white text-5xl font-extrabold">{data.name}</h1>
+        <h3 className="text-white text-xl my-2">{data.title}</h3>
+        <p className="text-white text-base mb-4 max-w-2xl">{data.lore}</p>
+        <h3 className="text-white text-2xl">스탯</h3>
         <ul role="list" className="text-white text-base list-disc pl-5 ">
           <li>공격력: {data.info.attack}</li>
           <li>방어력: {data.info.defense}</li>
